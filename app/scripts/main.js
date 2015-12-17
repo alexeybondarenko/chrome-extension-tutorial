765d61a9407e24b8f3b5082b7681b36d63904a4b
Alexey Bondarenko <alexeybondarenko@me.com>
2015-12-17 23:10:17 +0200

    quiz script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+(function () {
+  document.getElementById("test").quiz({
+    title: "Тест 2",	// type: string; default: "Тестування"
+    resTitle: "Результати",				// type: string; default: "Результати"
+    ansTitle: "Відповіді",				// type: string; default: "Відповіді"
+    type: "letter",								// type: string; default: "letter"; Values: letter, numbers
+    percent: true,									// type: boolean; default: true
+    points: true,									// type: boolean; default: false
+    debug: false									// type: boolean; default: false
+  });
+})();
